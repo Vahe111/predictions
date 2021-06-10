7142de37538b2d9ef7f866f4eab74a52c63374f6
Vahe Gevorgyan <vahe.gevorgyan@ifyoucan.com>
2021-06-10 12:17:58 +0400

    new auth controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,6 +19,11 @@ Route::get('/others', 'HomeController@others')->name('others');
 Route::get('/champion', 'HomeController@champion')->name('champion');
 Route::post('/save-champion', 'HomeController@saveChampion')->name('saveChampion');
 
-Route::auth();
+Route::get('/register', 'RegistrationController@create')->name('register');
+Route::post('register', 'RegistrationController@store');
+
+Route::get('/login', 'SessionsController@create');
+Route::post('/login', 'SessionsController@store');
+Route::get('/logout', 'SessionsController@destroy');
 
 Route::get('/home', 'HomeController@index');
